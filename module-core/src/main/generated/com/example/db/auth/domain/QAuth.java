f3fe26ee383267ebbb0910f150e06d81bc66e404
taiji5@kimyoung.co.kr <taiji5@kimyoung.kr>
2023-05-22 15:54:39 +0900

    재빌드 시 Q파일 위치변경

--- module-core/src/main/generated/com/example/db/auth/domain/QAuth.java
+++ module-core/src/main/generated/com/example/db/auth/domain/QAuth.java
@@ -1,8 +1,7 @@
-package com.example.user.domain;
+package com.example.db.auth.domain;
 
 import static com.querydsl.core.types.PathMetadataFactory.*;
 
-import com.example.db.user.domain.User;
 import com.querydsl.core.types.dsl.*;
 
 import com.querydsl.core.types.PathMetadata;
@@ -11,16 +10,18 @@ import com.querydsl.core.types.Path;
 
 
 /**
- * QUser is a Querydsl query type for User
+ * QAuth is a Querydsl query type for Auth
  */
 @Generated("com.querydsl.codegen.DefaultEntitySerializer")
-public class QUser extends EntityPathBase<User> {
+public class QAuth extends EntityPathBase<Auth> {
 
-    private static final long serialVersionUID = 1662407939L;
+    private static final long serialVersionUID = 711017323L;
 
-    public static final QUser user = new QUser("user");
+    public static final QAuth auth = new QAuth("auth");
 
-    public final StringPath id = createString("id");
+    public final NumberPath<Integer> authIdx = createNumber("authIdx", Integer.class);
+
+    public final StringPath authority = createString("authority");
 
     public final StringPath isDelete = createString("isDelete");
 
@@ -28,28 +29,22 @@ public class QUser extends EntityPathBase<User> {
 
     public final NumberPath<Integer> modifyIdx = createNumber("modifyIdx", Integer.class);
 
-    public final StringPath name = createString("name");
-
-    public final StringPath pwd = createString("pwd");
-
     public final DateTimePath<java.time.LocalDateTime> registerDate = createDateTime("registerDate", java.time.LocalDateTime.class);
 
     public final NumberPath<Integer> registerIdx = createNumber("registerIdx", Integer.class);
 
     public final StringPath role = createString("role");
 
-    public final NumberPath<Integer> userIdx = createNumber("userIdx", Integer.class);
-
-    public QUser(String variable) {
-        super(User.class, forVariable(variable));
+    public QAuth(String variable) {
+        super(Auth.class, forVariable(variable));
     }
 
-    public QUser(Path<? extends User> path) {
+    public QAuth(Path<? extends Auth> path) {
         super(path.getType(), path.getMetadata());
     }
 
-    public QUser(PathMetadata metadata) {
-        super(User.class, metadata);
+    public QAuth(PathMetadata metadata) {
+        super(Auth.class, metadata);
     }
 
 }
