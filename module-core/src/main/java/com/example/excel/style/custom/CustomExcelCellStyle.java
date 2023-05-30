d48fad93b315a9352746d0bbde1e24e5eb741bd6
itsiyeon@nate.com <itsiyeon@nate.com>
2023-05-30 09:11:30 +0900

    :pushpin: 엑셀 version 1.0

--- module-core/src/main/java/com/example/excel/style/custom/CustomExcelCellStyle.java
+++ module-core/src/main/java/com/example/excel/style/custom/CustomExcelCellStyle.java
@@ -3,20 +3,22 @@ package com.example.excel.style.custom;
 import com.example.excel.configurer.ExcelCellStyleConfigurer;
 import com.example.excel.style.ExcelCellStyle;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Workbook;
 
 public abstract class CustomExcelCellStyle implements ExcelCellStyle {
 
-	private ExcelCellStyleConfigurer configurer = new ExcelCellStyleConfigurer();
+	private final ExcelCellStyleConfigurer configurer;
 
 	public CustomExcelCellStyle() {
+		configurer = new ExcelCellStyleConfigurer();
 		configure(configurer);
 	}
 
 	public abstract void configure(ExcelCellStyleConfigurer configurer);
 
 	@Override
-	public void apply(CellStyle cellStyle) {
-		configurer.configure(cellStyle);
+	public void apply(CellStyle cellStyle, Workbook workbook) {
+		configurer.configure(cellStyle, workbook);
 	}
 
 }
